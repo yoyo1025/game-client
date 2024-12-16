2de4a1545c27ee68c5dca2cee28b7b3fbf322d6c
yoyo <hsjejxys@gmail.com>
2024-12-16 19:00:55 +0900

    ホバー時に説明を表示

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,27 @@
 import "../App.css";
 
 export default function Home() {
-  const handleClick = (message) => {
-    alert(message);
-  };
-
   return (
     <div>
       <div className="menu">
-        <button
-          className="menu-botton"
-          onClick={() => handleClick("ルールアイコンがクリックされました")}
-        >
-          <img src="rule-icon.png" className="menu-icon" alt="Rule Icon" />
-        </button>
-        <button
-          className="menu-botton"
-          onClick={() => handleClick("戦績アイコンがクリックされました")}
-        >
-          <img src="battle-record-icon.png" className="menu-icon" alt="Battle Record Icon" />
-        </button>
-        <button
-          className="menu-botton"
-          onClick={() => handleClick("ログアウトアイコンがクリックされました")}
-        >
-          <img src="logout-icon.png" className="menu-icon" alt="Logout Icon" />
-        </button>
+        <div className="menu-item">
+          <button className="menu-botton">
+            <img src="rule-icon.png" className="menu-icon" alt="Rule Icon" />
+          </button>
+          <div className="menu-description">ルール</div>
+        </div>
+        <div className="menu-item">
+          <button className="menu-botton">
+            <img src="battle-record-icon.png" className="menu-icon" alt="Battle Record Icon" />
+          </button>
+          <div className="menu-description">戦績</div>
+        </div>
+        <div className="menu-item">
+          <button className="menu-botton">
+            <img src="logout-icon.png" className="menu-icon" alt="Logout Icon" />
+          </button>
+          <div className="menu-description">ログアウト</div>
+        </div>
       </div>
       <div className="make-or-join-room">
         <div className="make-room-gourp">
