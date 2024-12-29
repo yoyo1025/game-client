48131cca8e7ddfbb4714a5440297e11bfb42da88
bp22008 <bp22008@shibaura-it.ac.jp>
2024-12-30 01:43:31 +0900

    アカウント登録画面作成。コンポーネントにRegister.jsx追加,App.css,App.js修正

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import RoomMake from "./components/RoomMake";
 import RoomJoin from "./components/RoomJoin";
+import Register from "./components/Register";
 
 export default function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/register" element={<Register />}/>
         <Route path="/room-make" element={<RoomMake />}/>
         <Route path="/room-join" element={<RoomJoin />}/>
       </Routes>
