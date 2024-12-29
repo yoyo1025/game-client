ab08aa384af0b2908f5f157f38424aefbbf62fba
yoyo <hsjejxys@gmail.com>
2024-12-29 23:13:20 +0900

    プレイヤーステータス欄のレイアウト変更

--- src/components/PlayerStatus.jsx
+++ src/components/PlayerStatus.jsx
@@ -4,50 +4,42 @@ export default function PlayerStatus() {
   return (
     <div className="player-status-container">
       <div className="player-status">
-        <div className="player-info">
-          <div style={{ display: "flex" }}>
-            <img className="status-icon" src="player1-status-icon.svg" alt="" />
-            <div className="player-name">プレイヤー1</div>
-          </div>
-          <div className="player-points">10pt</div>
+        <div className="player-upper-row">
+          <img className="status-icon" src="player1-status-icon.svg" alt="プレイヤー1アイコン" />
+          <div className="player-name">プレイヤー1</div>
         </div>
-        <div className="player-turn-indicator">
-          {/* <img className="is-playing" src="game-controller.svg"/> */}
+        <div className="player-lower-row">
+          <div className="player-points">4pt</div>
+          {/* <img className="is-playing" src="game-controller.svg" alt="操作中アイコン" /> */}
         </div>
       </div>
       <div className="player-status">
-        <div className="player-info">
-          <div style={{ display: "flex" }}>
-            <img className="status-icon" src="player2-status-icon.svg" alt="" />
-            <div className="player-name">プレイヤー2</div>
-          </div>
+        <div className="player-upper-row">
+          <img className="status-icon" src="player2-status-icon.svg" alt="プレイヤー2アイコン" />
+          <div className="player-name">プレイヤー2</div>
+        </div>
+        <div className="player-lower-row">
           <div className="player-points">12pt</div>
-        </div>
-        <div className="player-turn-indicator">
-          <img className="is-playing" src="game-controller.svg"/>
+          <img className="is-playing" src="game-controller.svg" alt="操作中アイコン" />
         </div>
       </div>
       <div className="player-status">
-        <div className="player-info">
-          <div style={{ display: "flex" }}>
-            <img className="status-icon" src="player3-status-icon.svg" alt="" />
-            <div className="player-name">プレイヤー3</div>
-          </div>
-          <div className="player-points">8pt</div>
+        <div className="player-upper-row">
+          <img className="status-icon" src="player3-status-icon.svg" alt="プレイヤー3アイコン" />
+          <div className="player-name">プレイヤー3</div>
         </div>
-        <div className="player-turn-indicator">
-          {/* <img className="is-playing" src="game-controller.svg"/> */}
+        <div className="player-lower-row">
+          <div className="player-points">10pt</div>
+          {/* <img className="is-playing" src="game-controller.svg" alt="操作中アイコン" /> */}
         </div>
       </div>
       <div className="player-status">
-        <div className="player-info">
-          <div style={{ display: "flex" }}>
-            <img className="status-icon" src="demon-status-icon.svg" alt="" />
-            <div className="player-name">プレイヤー4</div>
-          </div>
+        <div className="player-upper-row">
+          <img className="status-icon" src="demon-status-icon.svg" alt="鬼アイコン" />
+          <div className="player-name">プレイヤー4</div>
         </div>
-        <div className="player-turn-indicator">
-          {/* <img className="is-playing" src="game-controller.svg"/> */}
+        <div className="player-lower-row">
+          {/* <img className="is-playing" src="game-controller.svg" alt="操作中アイコン" /> */}
         </div>
       </div>
     </div>
