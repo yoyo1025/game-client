68a5a2e4e618cecf996569891d752d65c35dbf34
yoyo <hsjejxys@gmail.com>
2024-12-29 21:44:02 +0900

    だれが操作中かをアイコンを用いて表示

--- src/components/PlayerStatus.jsx
+++ src/components/PlayerStatus.jsx
@@ -1,3 +1,5 @@
+import "../Game.css";
+
 export default function PlayerStatus() {
   return (
     <div className="player-status-container">
@@ -6,28 +8,35 @@ export default function PlayerStatus() {
           <div className="player-name">プレイヤー1</div>
           <div className="player-points">10pt</div>
         </div>
-        <div className="player-turn-indicator">○</div>
+        <div className="player-turn-indicator">
+          <img src="game-controller.svg"/>
+        </div>
       </div>
       <div className="player-status">
         <div className="player-info">
           <div className="player-name">プレイヤー2</div>
           <div className="player-points">12pt</div>
         </div>
-        <div className="player-turn-indicator"></div>
+        <div className="player-turn-indicator">
+          <img src="game-controller.svg"/>
+        </div>
       </div>
       <div className="player-status">
         <div className="player-info">
           <div className="player-name">プレイヤー3</div>
           <div className="player-points">8pt</div>
         </div>
-        <div className="player-turn-indicator"></div>
+        <div className="player-turn-indicator">
+        <img src="game-controller.svg"/>
+        </div>
       </div>
       <div className="player-status">
         <div className="player-info">
           <div className="player-name">プレイヤー4</div>
-          <div className="player-points">3pt</div>
         </div>
-        <div className="player-turn-indicator"></div>
+        <div className="player-turn-indicator">
+          <img src="game-controller.svg"/>
+        </div>
       </div>
     </div>
   );
