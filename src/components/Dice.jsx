58f78459aba3d6122e0a8b049384b3a7fe327e47
yoyo <hsjejxys@gmail.com>
2024-12-29 23:50:03 +0900

    サイコロの値を変更

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -3,7 +3,7 @@ import '../Game.css';
 export default function Dice(){
   return (
     <button className='dice'>
-      <img className='dice-picture' src="./dice.png"/>
+      <img className='dice-picture' src="./dice1.svg"/>
     </button>
   )
 }
