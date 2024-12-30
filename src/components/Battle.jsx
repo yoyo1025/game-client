2cf24eafcbb30f9942177dd8da51a5d467b0b4c9
yoyo <hsjejxys@gmail.com>
2024-12-30 23:20:48 +0900

    api/start-gameからJSON取得

--- src/components/Battle.jsx
+++ src/components/Battle.jsx
@@ -2,8 +2,29 @@ import Field from "./Field";
 import Dice from "./Dice";
 import Event from "./Event";
 import PlayerStatus from "./PlayerStatus";
+import { useEffect } from "react";
 
 export default function Battle() {
+
+  const fetchGameState = async () => {
+    try {
+      const res = await fetch('http://localhost:8080/api/start-game', {
+        method: 'GET',
+      });
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching game data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchGameState();
+  }, []);
+
   return(
     <div className="battle-screen">
       <div className="dice-event">
