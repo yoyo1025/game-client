66348433b9747e034ff8472f439262060ac47abb
bp22072 <bp22072@shibaura-it.ac.jp>
2025-01-03 17:22:33 +0900

    入力フォームを追加

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -19,4 +19,35 @@ const LoginForm = () =>{
     
 }
 
-return
+return(
+   <div style={styles.container}>
+    //ゲームタイトルロゴ
+    <img src="" alt="Game Title" style={styles.gameTitleImage}/>
+
+    <form onSubmit={handleLogin} style={styles.form}>
+        <label htmlFor="userId">ユーザーID</label>
+        <div style={styles.inputContainer}>
+        <input
+        id="userId"
+        type="text"
+        value={userId}
+        onChange={(e) => setUserId(e.target.value)}
+        required
+        style={style.input}
+        />
+        </div>
+        <div style={styles.inputContainer}>
+            <label htmlFor="password">パスワード</label>
+            <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            style={styles.input}
+            />
+        </div>
+    </form>
+
+   </div> 
+)
