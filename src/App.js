a8f45a178e1b0d9069dd19edd604c6219fb86183
bp22072 <bp22072@shibaura-it.ac.jp>
2025-01-07 01:07:36 +0900

    Loginのルートを追加

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import RoomMake from "./components/RoomMake";
 import RoomJoin from "./components/RoomJoin";
+import Login from "./components/Login"
+import React from "react";
 
 export default function App() {
   return (
@@ -10,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/room-make" element={<RoomMake />}/>
         <Route path="/room-join" element={<RoomJoin />}/>
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   )
