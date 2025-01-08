6b9e36a311065b7a28a7c4115315ecb4257736c6
bp22008 <bp22008@shibaura-it.ac.jp>
2025-01-08 13:32:34 +0900

    アカウント登録完了画面

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import RoomMake from "./components/RoomMake";
-import RoomJoin from "./components/RoomJoin";
+import RoomJoin from "./components/RoomJoin"
+import RegisterComplete from "./components/RegisterComplete"
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/room-make" element={<RoomMake />}/>
         <Route path="/room-join" element={<RoomJoin />}/>
+        <Route path="/register-complete" element={<RegisterComplete />}/>
       </Routes>
     </div>
   )
