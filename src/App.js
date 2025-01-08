457b52bb43cf6f0887c55876554249821b7d755f
bp22072 <bp22072@shibaura-it.ac.jp>
2025-01-08 13:56:21 +0900

    画面遷移を追加

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import RoomMake from "./components/RoomMake";
 import RoomJoin from "./components/RoomJoin";
+import Logout from "./components/Logout";
+import Result from "./components/Result";
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/room-make" element={<RoomMake />}/>
         <Route path="/room-join" element={<RoomJoin />}/>
+        <Route path="/logout" element={<Logout />}/>
+        <Route path="/battleresult" element={<Result />}/>
       </Routes>
     </div>
   )
