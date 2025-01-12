ff272c1eb83f6b6e8dcc36c58341dca4245f8688
naoto08 <150799588+naoto08@users.noreply.github.com>
2025-01-12 21:15:14 +0900

    Revert "ログイン画面のバックエンドリクエスト機能を実装"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import RoomMake from "./components/RoomMake";
-import RoomJoin from "./components/RoomJoin"
-import RegisterComplete from "./components/RegisterComplete"
-import Login from "./components/Login"
-import React from "react";
+import RoomJoin from "./components/RoomJoin";
 import Register from "./components/Register";
 import Battle from "./components/Battle";
 import PlayerStatus from "./components/PlayerStatus";
@@ -17,8 +14,6 @@ export default function App() {
         <Route path="/register" element={<Register />}/>
         <Route path="/room-make" element={<RoomMake />}/>
         <Route path="/room-join" element={<RoomJoin />}/>
-        <Route path="/register-complete" element={<RegisterComplete />}/>
-        <Route path="/login" element={<Login />} />
         <Route path="/battle" element={<Battle/>}/>
         <Route path="/player-status" element={<PlayerStatus/>}/>
       </Routes>
